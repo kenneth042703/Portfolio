5948dd20e72f8b4008fc23189682b46357b20aa3
kenneth042703 <asakura.ritualo1@gmail.com>
2021-10-06 03:34:27 +0800

    changes

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const links = document.querySelectorAll(".scroll_to");
 links.forEach((item) => {
     item.addEventListener("click", () => {
         const el = document.getElementById(item.getAttribute("data-link"));
-        el.scrollIntoView({block:"start"})
+        el.scrollIntoView({behavior:"smooth", block:"start"})
     })
 })
